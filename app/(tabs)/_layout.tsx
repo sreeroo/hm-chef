11980a2490bd4e67b3a22605e5a591cb6b1f76cf
Sreeroop <sreeroop.adukkadukkam@hm.edu>
2025-04-12 22:07:09 +0200

    feat: create tab pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,14 +6,8 @@ import { Pressable } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { MaterialIcons } from '@expo/vector-icons';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -34,7 +28,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons name="home" color={color} />,
           headerTitle: 'Crazy HM Recipes',
         }}
       />
@@ -42,21 +36,21 @@ export default function TabLayout() {
         name="search"
         options={{
           title: 'Search',
-          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons name="search" color={color} />,
         }}
       />
       <Tabs.Screen
       name="create"
       options={{
         title: 'New Recipe',
-        tabBarIcon: ({color}) => <TabBarIcon name="plus-square" color="{color}"/>,
+        tabBarIcon: ({color}) => <MaterialIcons name="add" color="{color}"/>,
       }}
       />
       <Tabs.Screen
-      name="profile"
+      name="favorite"
       options={{
         title: 'My Recipes',
-        tabBarIcon: ({color}) => <TabBarIcon name='user' color="{color}"/>,
+        tabBarIcon: ({color}) => <MaterialIcons name='favorite' color="{color}"/>,
       }}
       />
     </Tabs>
